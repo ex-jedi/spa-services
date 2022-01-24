b6d744441087186b0a643ba0de9154a73daae76d
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-01-24 13:16:38 +0000

    Services link section

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php perch_layout('heads/main-head'); ?>
 <?php perch_layout('headers/homepage-header'); ?>
 <main class="main-content homepage-main-content" id="main-content">
-	  <?php perch_content('Placeholder'); ?>
+	  <?php perch_content('Homepage Services Link Section'); ?>
 </main>
 <?php perch_layout('footers/main-footer'); ?>
 <?php perch_layout('footer-ends/main-footer-end'); ?>
