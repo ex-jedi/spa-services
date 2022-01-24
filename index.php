311c6822ee2b71f620a96c80d1551f69128d2d1f
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-01-24 12:26:34 +0000

    Header content

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <?php perch_layout('heads/main-head'); ?>
 <?php perch_layout('headers/homepage-header'); ?>
 <main class="main-content homepage-main-content" id="main-content">
-<h1>Ahoy</h1>
 	  <?php perch_content('Placeholder'); ?>
 </main>
 <?php perch_layout('footers/main-footer'); ?>
