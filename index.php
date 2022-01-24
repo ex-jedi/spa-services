00dbb0d35ccd355109081966e0620e15c9b40772
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-01-24 09:29:28 +0000

    Main logo

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include('perch/runtime.php'); ?>
 <?php perch_layout('heads/main-head'); ?>
-<?php perch_layout('headers/main-header'); ?>
+<?php perch_layout('headers/homepage-header'); ?>
 <main class="main-content homepage-main-content" id="main-content">
 <h1>Ahoy</h1>
 	  <?php perch_content('Placeholder'); ?>
