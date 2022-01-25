bb027580aa117e9cdcb34bd94bdb34048558c64c
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-01-25 10:08:50 +0000

    Phone filter

--- perch/templates/layouts/footers/main-footer.php
+++ perch/templates/layouts/footers/main-footer.php
@@ -1,4 +1,7 @@
 <footer class="main-footer">
+		<?php perch_content("Footer Opening Hours"); ?>
+		<?php perch_content("Footer Address"); ?>
+		<?php perch_content("Footer Contact Details"); ?>
 		<?php perch_content("Footer Social Media"); ?>
 		<p class="footer-privacy">
 			<a href="/privacy-and-cookie-policy.php">Privacy &amp; cookie policy</a>
@@ -6,11 +9,11 @@
 		<p class="footer-terms">
 			<a href="/terms.php">Terms of use</a>
 		</p>
-		<p class="footer-copyright" >
-			&copy; <?php echo date("Y"); ?> <span itemprop="name">Spa Service</span>
-		</p>
 		<p class="footer-credit">
 			Web design & development, brand design & illustration by <a href="" class="promo-link">phoenix & phoenix</a>
+		</p>
+		<p class="footer-copyright" >
+			&copy; <?php echo date("Y"); ?> <span itemprop="name">Spa Service</span>
 		</p>
 </footer>
 
