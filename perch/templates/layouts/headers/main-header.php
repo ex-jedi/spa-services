4907ff6d2db3d19dd451ef5d02304426def633e0
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-01-25 12:45:09 +0000

    Services header

--- perch/templates/layouts/headers/main-header.php
+++ perch/templates/layouts/headers/main-header.php
@@ -1,22 +1,16 @@
 <!-- Anything to include in head goes here  -->
 </head>
-	<body class="<?php perch_page_attribute('bodyClass', array('template' => 'attribute_files/bits.html' )); ?>">
+	<body class="<?php perch_page_attribute('bodyClass', array('template' => 'bits.html' )); ?>">
 <!-- Cookie Warning -->
 <?php perch_content("Cookie Warning"); ?>
-	<?php perch_content("Header Content"); ?>
 	<div class="site-wrapper" itemscope itemtype="http://schema.org/LocalBusiness">
-		<header class="header main-header">
+		<header class="header main-header homepage-header">
 			<!-- Skip to main content link  -->
 			<a class="show-on-focus"  href="#main-content">Skip to main content</a>
-			<!-- For SVG logo. Delete if not needed -->
-			<?php perch_content("Hidden Logo"); ?>
-			<div class="main-logo-wrapper">
-				<a class="main-logo-link"  href="/">
-				<?php perch_content("Main Logo"); ?>
-			</a>
-			</div>
+			<?php perch_content("Main Logo"); ?>
 			<?php perch_pages_navigation(array(
 			'hide-extensions' => true,
 			'levels'    => 1
 			)); ?>
+			<?php perch_content("Header Content"); ?>
 		</header>
--- services.php
+++ services.php
@@ -1,7 +1,7 @@
 <?php include('perch/runtime.php'); ?>
 <?php perch_layout('heads/main-head'); ?>
 <?php perch_layout('headers/main-header'); ?>
-<main class="main-content services-main-content" id="main-content">
+<main class="main-content pages-main-content services-main-content" id="main-content">
 	  <?php perch_content('Page Content'); ?>
 </main>
 <?php perch_layout('footers/main-footer'); ?>
