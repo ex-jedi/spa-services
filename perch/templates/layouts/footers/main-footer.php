62e9ff85dfc2ab85d2650e6f9d535153be0ac8e2
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-01-25 11:17:11 +0000

    Footer styling

--- perch/templates/layouts/footers/main-footer.php
+++ perch/templates/layouts/footers/main-footer.php
@@ -3,18 +3,22 @@
 		<?php perch_content("Footer Address"); ?>
 		<?php perch_content("Footer Contact Details"); ?>
 		<?php perch_content("Footer Social Media"); ?>
-		<p class="footer-privacy">
-			<a href="/privacy-and-cookie-policy.php">Privacy &amp; cookie policy</a>
-		</p>
-		<p class="footer-terms">
-			<a href="/terms.php">Terms of use</a>
-		</p>
-		<p class="footer-credit">
-			Web design & development, brand design & illustration by <a href="" class="promo-link">phoenix & phoenix</a>
-		</p>
-		<p class="footer-copyright" >
-			&copy; <?php echo date("Y"); ?> <span itemprop="name">Spa Service</span>
-		</p>
+		<div class="footer-legals-wrapper">
+			<p class="footer-privacy">
+				<a href="/privacy-and-cookie-policy.php">Privacy &amp; cookie policy</a>
+			</p>
+			<p class="footer-terms">
+				<a href="/terms.php">Terms of use</a>
+			</p>
+			<p class="footer-copyright" >
+				&copy; <?php echo date("Y"); ?> <span itemprop="name">Spa Service</span>
+			</p>
+		</div>
+		<div class="footer-credit-wrapper">
+			<p class="footer-credit">
+				Web design & development, brand design & illustration by <a href="" class="promo-link">phoenix & phoenix</a>
+			</p>
+		</div>
 </footer>
 
 </div> <!-- wrapper div -->
