28edd156963ed6519a1f28fc41e3532f57065b88
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-01-26 13:52:16 +0000

    Sitemap corrections

--- sitemap.php
+++ sitemap.php
@@ -8,16 +8,5 @@ perch_pages_navigation(array(
      'flat' => true,
      'hide-extensions' => true
  ));
-     perch_blog_custom(array(
-     'template' => 'sitemap-blog.html',
-     'sort'=>'postDateTime',
-     'sort-order'=>'DESC',
-     'count' => 3000
- ));
- perch_blog_categories(array(
-     'sort' => 'catSlug',
-     'sort=order'=> 'ASC',
-     'template'=> 'sitemap-category.html'
- ));
  echo '</urlset>';
  ?>
