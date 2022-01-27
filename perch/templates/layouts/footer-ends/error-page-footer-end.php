82d259dbdb6961442caea2cbe66a2baeff399c77
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-01-27 17:25:11 +0000

    Deleting unused Perch templates

--- perch/templates/layouts/footer-ends/error-page-footer-end.php
+++
@@ -1,4 +0,0 @@
-<!-- JS file -->
-<script src="/scripts/minified/error-page-min.js?v=<?= filemtime($_SERVER['DOCUMENT_ROOT'].'/scripts/minified/error-page-min.js');?>"></script>
-</body>
-</html>
--- perch/templates/layouts/footer-ends/legals-footer-end.php
+++
@@ -1,4 +0,0 @@
-<!-- JS file -->
-<script src="/scripts/minified/legals-min.js?v=<?= filemtime($_SERVER['DOCUMENT_ROOT'].'/scripts/minified/legals-min.js');?>"></script>
-</body>
-</html>
